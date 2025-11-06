d17e0f3ca2e5dd463c0a524fbff81e2d5ddbada5
soad Albasha <soadalbasha3@gmail.com>
2025-11-06 20:51:56 +0300

    ربط زر المقارنة بصفحة compare.html

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// قراءة الكلمة من الرابط
+const params = new URLSearchParams(window.location.search);
+const query = params.get("search");
+
+// عرض الكلمة في العنوان
+if (query) {
+  document.getElementById("page-title").innerHTML = `نتائج البحث عن: ${query}`;
+}
+
 function searchProducts() {
   const query = document.getElementById("search").value;
   const list = document.getElementById("product-list");
